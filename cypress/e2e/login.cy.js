e9262061cc7a7dd6682c908e653a3a05651f9af3
Arthur Gonçalves <arthurcorreia50@gmail.com>
2025-07-06 19:47:05 -0300

    Refatorando testes de login e criando fixtures

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,23 +1,19 @@
+import loginData from "../fixtures/login.json"
 describe('Login', () => {
   beforeEach(()=>{
     cy.acessarHome()
   })
   it('loginSucesso', () => {
-    const usuario = {
-      email: "teste.qa@gmail.com",
-      senha: "Teste@123!",
-    }
-    cy.preencherFormLogin(usuario.email, usuario.senha)
+    const usuario = loginData.perfilGestao
+    cy.preencherFormLogin(usuario.email, usuario.password)
     cy.submitBtn("Acessar")
     cy.verificarMsgToast("Login realizado com sucesso!")
+    cy.verificarUsuarioLogado(usuario.name)
   })
 
   it('loginSenhaInvalida', () => {
-    const usuario = {
-      email: "teste.qa@gmail.com",
-      senha: "Teste123!",
-    }
-    cy.preencherFormLogin(usuario.email, usuario.senha)
+    const usuario = {...loginData.perfilGestao, password: "Teste123"}
+    cy.preencherFormLogin(usuario.email, usuario.password)
     cy.submitBtn("Acessar")
     cy.verificarMsgToast("Credenciais inválidas. Verifique seu e-mail e senha.")
   })
@@ -26,5 +22,13 @@ describe('Login', () => {
     cy.submitBtn("Acessar")
     cy.verificarMsgErro("O campo de e-mail é obrigatório.")
     cy.verificarMsgErro("O campo de senha é obrigatório.")
+  })
+
+  it('loginSalao', () => {
+    const usuario = loginData.perfilSalao
+    cy.preencherFormLogin(usuario.email, usuario.password)
+    cy.submitBtn("Acessar")
+    cy.verificarMsgToast("Acesse através do app.")
+    cy.verificarPagina("app-info", "Acesso pelo APP E2E Burguer")
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,26 +1,39 @@
-Cypress.Commands.add('acessarHome', ()=>{
+Cypress.Commands.add('acessarHome', () => {
     cy.visit('/')
     cy.url().should('include', '/')
     cy.contains('h1', 'Faça seu login')
 })
 
-Cypress.Commands.add('preencherFormLogin', (email, senha)=>{
+Cypress.Commands.add('preencherFormLogin', (email, password) => {
     cy.get('#email').type(email);
-    cy.get('#password').type(senha);
+    cy.get('#password').type(password);
 })
 
-Cypress.Commands.add('submitBtn', (textoBtn)=>{
+Cypress.Commands.add('submitBtn', (textoBtn) => {
     cy.contains("button[type=submit]", textoBtn).click();
 })
 
-Cypress.Commands.add('verificarMsgToast', (msgEsperada)=>{
+Cypress.Commands.add('verificarMsgToast', (msgEsperada) => {
     cy.get(".Toastify__toast-body")
-    .should('be.visible')
-    .and("have.text", msgEsperada)
+        .should('be.visible')
+        .and("have.text", msgEsperada)
 })
-Cypress.Commands.add('verificarMsgErro', (msgEsperada)=>{
+Cypress.Commands.add('verificarMsgErro', (msgEsperada) => {
     cy.get("[class^='home_errorText']")
-    .should('be.visible')
-    .and("contain.text", msgEsperada)
-    .and("have.css", 'color', 'rgb(230, 57, 70)')
+        .should('be.visible')
+        .and("contain.text", msgEsperada)
+        .and("have.css", 'color', 'rgb(230, 57, 70)')
+})
+
+Cypress.Commands.add('verificarPagina', (rota, tituloPagina) => {
+    cy.url().should('include', `${rota}`)
+    cy.contains('h1', tituloPagina)
+})
+
+Cypress.Commands.add('verificarUsuarioLogado', (name) => {
+    const nomeUsuario = name.split(" ")[0]
+
+    cy.get('[data-testid="user-greeting"]')
+        .should("be.visible")
+        .and("contain.text", `Olá, ${nomeUsuario}!`)
 })
